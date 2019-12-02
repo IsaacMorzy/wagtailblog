4f4411f73ace8386a56dc99ef67a085416c49ead
Zack <zack@localhost.localdomain>
2019-12-02 19:34:30 +0300

    add list block  for repeating StreamField content (Cards)

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,6 +1,7 @@
-
+"""Streamfields live in here."""
 
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -15,7 +16,30 @@ class TitleAndTextBlock(blocks.StructBlock):
         label = "Title & Text"
 
 
-class RichTextBlock(blocks.RichTextBlock):
+class CardBlock(blocks.StructBlock):
+    """Cards with image and text and button(s)."""
+
+    title = blocks.CharBlock(required=True, help_text="Add your title")
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(required=True, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False, help_text="If the button page above is selected, that will be used first.")),
+            ]
+        )
+    )
+
+    class Meta:  # noqa
+        template = "streams/card_block.html"
+        icon = "placeholder"
+        label = "Staff Cards"
+
+
+class RichtextBlock(blocks.RichTextBlock):
     """Richtext with all the features."""
 
     class Meta:  # noqa
@@ -24,7 +48,7 @@ class RichTextBlock(blocks.RichTextBlock):
         label = "Full RichText"
 
 
-class SimpleRichTextBlock(blocks.RichTextBlock):
+class SimpleRichtextBlock(blocks.RichTextBlock):
     """Richtext without (limited) all the features."""
 
     def __init__(
