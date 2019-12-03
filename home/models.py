b1ec07411bfc703f54cf1ba19d364ea5a74312d6
Zack <zack@localhost.localdomain>
2019-12-03 11:48:19 +0300

    Add CTA Streamfield on home & flex page

--- home/models.py
+++ home/models.py
@@ -1,44 +1,53 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, PageChooserPanel
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel,PageChooserPanel
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
 
 
 class HomePage(Page):
     """Home page model."""
-   
+
     template = "home/home_page.html"
-    banner_title = models.CharField(max_length=100,blank=False,null=True)
-    banner_subtitle=RichTextField(features=["bold","italic"])
+    max_count = 1
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
     banner_image = models.ForeignKey(
         "wagtailimages.Image",
         null=True,
-        blank=True,
+        blank=False,
         on_delete=models.SET_NULL,
-        related_name='+')
-
-    banner_cta=models.ForeignKey(
+        related_name="+"
+    )
+    banner_cta = models.ForeignKey(
         "wagtailcore.Page",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+')
-    
-    body = RichTextField(blank=True)
-    max_count = 1
-    
+        related_name="+"
+    )
+
+    content = StreamField(
+        [
+            ("cta", blocks.CTABlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
     content_panels = Page.content_panels + [
-        FieldPanel("body", classname="full"),
         FieldPanel("banner_title"),
         FieldPanel("banner_subtitle"),
-        ImageChooserPanel('banner_image'),
-        PageChooserPanel('banner_cta'),
-
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta"),
+        StreamFieldPanel("content"),
     ]
-   
+
     class Meta:
 
-        verbose_name="Home Page"
-        verbose_name_plural="Home Pages"
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
