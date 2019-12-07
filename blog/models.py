71cdac4f0c7d7ff9240b53e5a12ddbbcfe86a093
IsaacMorzy <musyokaisaac98@gmail.com>
2019-12-07 12:55:09 +0300

    Add Blog Listing Page, Blog Detail Pages &  Custom Context

--- blog/models.py
+++ blog/models.py
@@ -1,31 +1,68 @@
+"""Blog listing and blog detail pages."""
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.search import index
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
 
 
-# Keep the definition of BlogIndexPage, and add:
-class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
-    content_panels = Page.content_panels + [
-    FieldPanel('intro', classname="full")
-    ]
+class BlogListingPage(Page):
+    """Listing page lists all the Blog Detail Pages."""
 
+    template = "blog/blog_listing_page.html"
 
-class BlogPage(Page):
-    date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
-
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Overwrites the default title',
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
+        FieldPanel("custom_title"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        context["posts"] = BlogDetailPage.objects.live().public()
+        return context
+
+
+class BlogDetailPage(Page):
+    """Blog detail page."""
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Overwrites the default title',
+    )
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=False,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        ImageChooserPanel("blog_image"),
+        StreamFieldPanel("content"),
     ]
